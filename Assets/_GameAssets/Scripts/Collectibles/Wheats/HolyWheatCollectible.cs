618a517589428270f0505adc903f7c882c79bba5
Rumeysa AY <rumeeysaay@gmail.com>
2025-06-18 13:14:19 +0300

    v0.10 Boostable Spatula Added

--- Assets/_GameAssets/Scripts/Collectibles/Wheats/HolyWheatCollectible.cs
+++ Assets/_GameAssets/Scripts/Collectibles/Wheats/HolyWheatCollectible.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class HolyWheatCollectible : MonoBehaviour, ICollectible
 {
-    [SerializeField] private PlayerController _playerController;
+    [SerializeField] private WheatDesignSO _wheatDesignSO;
 
-    [SerializeField] private float _forceIncrease;
-    [SerializeField] private float _resetBoostDuration;
+    [SerializeField] private PlayerController _playerController;
 
     public void Collect()
     {
-        _playerController.SetJumpForce(_forceIncrease, _resetBoostDuration);
+        _playerController.SetJumpForce(_wheatDesignSO.IncreaseDecreaseMultiplier, _wheatDesignSO.ResetBoostDuration);
         Destroy(gameObject);
     }
 }
